4a3bd633f75174004763b5e68f65e08778f06bd7
Philipp Bohnenstengel <coding@phb23.io>
2025-01-24 23:46:04 +0100

    score label

--- main.gd
+++ main.gd
@@ -1,10 +1,15 @@
 extends Node
 
 @onready var sphere = $Sphere
+@onready var scores = $UI/Scores
 
+var sphereInitialRadius = 10 # how to get this programmatically?
 var sphereShrinkRate = 0.999
 
 func _on_tick_timeout():
-	var scaleBefore = sphere.scale
+	var scaleBefore = sphere.scale	
 	var scaleAfter = scaleBefore * Vector3(sphereShrinkRate, sphereShrinkRate, sphereShrinkRate)
 	sphere.scale = scaleAfter
+	
+	var sphereArea = sphere.scale.x * sphereInitialRadius * sphereInitialRadius * PI
+	scores.text = "Bubble Area: " + str(round(sphereArea)) + "m²"
