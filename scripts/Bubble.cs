2a1fcab792e923a5b4ae398ee5a7ee9a45c94766
PassM <pascal.maier2@student.kit.edu>
2025-01-25 20:26:29 +0100

    destory Bubble animation

---
+++ scripts/Bubble.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Bubble : StaticBody3D
+{
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	public void  destoryBubble(){
+		AnimationPlayer player = GetNode<AnimationPlayer>("MeshInstance3D/AnimationPlayer");
+		player.Play("DestroyBubble");
+	}	
+}
